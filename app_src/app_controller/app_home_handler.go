9c0863f8b3454045b63918266e51581dcff99f92
Avi Krishnan <cosmiczilch@gmail.com>
2020-04-07 23:33:32 +0530

    Add app_routes.go

--- app_src/app_controller/app_home_handler.go
+++ app_src/app_controller/app_home_handler.go
@@ -2,6 +2,7 @@ package app_controller
 
 import (
     "fmt"
+    "github.com/spacetimi/passman_server/app_src/app_routes"
     "github.com/spacetimi/timi_shared_server/code/core/controller"
     "github.com/spacetimi/timi_shared_server/utils/logger"
     "net/http"
@@ -13,10 +14,9 @@ type AppHomeHandler struct {     // Implements IRouteHandler
 }
 
 func (ahh *AppHomeHandler) Routes() []controller.Route {
-    routes := []controller.Route {
-        controller.NewRoute("/home", []controller.RequestMethodType{controller.GET, controller.POST}),
+    return []controller.Route {
+        controller.NewRoute(app_routes.Home, []controller.RequestMethodType{controller.GET, controller.POST}),
     }
-    return routes
 }
 
 func (ahh *AppHomeHandler) HandlerFunc(httpResponseWriter http.ResponseWriter, request *http.Request, args *controller.HandlerFuncArgs) {
---
+++ app_src/app_routes/app_routes.go
@@ -0,0 +1,3 @@
+package app_routes
+
+const Home = "/home"
