48acc3b51c43f1f34aeefd4dae99da9cc062d1f2
Avi Krishnan <cosmiczilch@gmail.com>
2020-06-09 22:45:26 +0530

    Minor: update dockerrun.sh to specify correct env

--- dockerrun.sh
+++ dockerrun.sh
@@ -4,6 +4,6 @@
 if [ $1 = "Local" ] || [ $1 = "Production" ]; then
    docker run --restart always -d -v $HOME/.aws:/root/.aws -e app_environment=$1 -e app_name=passman -e app_dir_path=/go/src/github.com/spacetimi/passman_server --publish 9000:9000 passman-server | xargs -I containerId docker logs -f containerId
 else
-   echo "Usage: dockerrun.sh <Local|Test>"
+   echo "Usage: dockerrun.sh <Local|Production>"
 fi
 
