085049c4f0ed315bf78fdd739d8e510d2e3f08db
Avi Krishnan <cosmiczilch@gmail.com>
2020-07-26 06:44:10 +0530

    Send files from client with file name and a password to encrypt the file with

--- app_src/app_route_handlers/home/handle_add_file_to_vault.go
+++ app_src/app_route_handlers/home/handle_add_file_to_vault.go
@@ -2,30 +2,38 @@ package home
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"net/http"
 
+	"github.com/spacetimi/passman_server/app_src/app_routes"
+	"github.com/spacetimi/passman_server/app_src/app_utils/app_simple_message_page"
 	"github.com/spacetimi/timi_shared_server/code/core/controller"
 	"github.com/spacetimi/timi_shared_server/code/core/services/identity_service"
 	"github.com/spacetimi/timi_shared_server/utils/logger"
 )
 
+const kAddFilePostArgFileName = "newFileName"
+const kAddFilePostArgFilePassword = "newFilePassword"
+
 func (hh *HomeHandler) handleAddFileToVault(user *identity_service.UserBlob, httpResponseWriter http.ResponseWriter, request *http.Request, args *controller.HandlerFuncArgs) {
-	err := request.ParseMultipartForm(32 << 20)
+
+	parsedArgs, err := parseAddNewFilePostArgs(args.PostArgs)
 	if err != nil {
-		logger.LogError("error parsing request for adding file to vault" +
-			"|error=" + err.Error())
-		// TODO: Add file name to logger here
-		httpResponseWriter.WriteHeader(http.StatusInternalServerError)
+		// Show error message and return
+		messageHeader := "Something went wrong"
+		messageBody := err.Error()
+		backlinkName := "<< Home"
+		app_simple_message_page.ShowAppSimpleMessagePage(httpResponseWriter, messageHeader, messageBody, app_routes.HomeSlash, backlinkName)
 		return
 	}
+	logger.VarDumpInfo("file name", parsedArgs.FileName)
 
 	var buffer bytes.Buffer
-	uploadedFile, _, err := request.FormFile("fileForVault")
+	uploadedFile, _, err := request.FormFile("newFile")
 	if err != nil {
 		logger.LogError("error getting file from request for adding to vault" +
 			"|error=" + err.Error())
-		// TODO: Add file name to logger here
 		httpResponseWriter.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -37,7 +45,6 @@ func (hh *HomeHandler) handleAddFileToVault(user *identity_service.UserBlob, htt
 	if err != nil {
 		logger.LogError("error copying file contents from request for adding to vault" +
 			"|error=" + err.Error())
-		// TODO: Add file name to logger here
 		httpResponseWriter.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -46,4 +53,27 @@ func (hh *HomeHandler) handleAddFileToVault(user *identity_service.UserBlob, htt
 	logger.VarDumpInfo("uploaded file contents", fileContents)
 
 	http.Redirect(httpResponseWriter, request, "/", http.StatusOK)
+}
+
+func parseAddNewFilePostArgs(postArgs map[string]string) (*AddNewFilePostArgs, error) {
+
+	fileName, ok := postArgs[kAddFilePostArgFileName]
+	if !ok || len(fileName) == 0 {
+		return nil, errors.New("* file name cannot be empty")
+	}
+
+	filePassword, ok := postArgs[kAddFilePostArgFilePassword]
+	if !ok || len(filePassword) == 0 {
+		return nil, errors.New("* file password cannot be empty")
+	}
+
+	return &AddNewFilePostArgs{
+		FileName:     fileName,
+		FilePassword: filePassword,
+	}, nil
+}
+
+type AddNewFilePostArgs struct {
+	FileName     string
+	FilePassword string
 }
